8da17a1c281cea99d46d1832eb2f99cc0dfa6123
Asha CM <ashacm@agileblaze.com>
2012-12-27 10:35:49 +0600

    Change in file in repo branch 7-1-1.

--- file_1.php
+++ file_1.php
@@ -7,3 +7,4 @@ echo "Fifth change in file in repo.";
 echo "Seventh change in file in repo.";
 echo "Eight change in file in repo.";
 echo "Ninth change in file in repo.";
+echo "Change in file in repo branch 7-1-1.";
